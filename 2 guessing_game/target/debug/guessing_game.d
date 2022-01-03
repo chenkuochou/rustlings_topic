1903acd584059b95f7df7e6583d5a22046d17d8d
Chen Chou <39699466+chenkuochou@users.noreply.github.com>
2022-01-03 23:02:34 +1030

    .

---
+++ 2 guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\Graph\Desktop\rustlings_topic\2\ guessing_game\target\debug\guessing_game.exe: C:\Users\Graph\Desktop\rustlings_topic\2\ guessing_game\src\main.rs
---
+++ 2 guessing_game/target/rls/debug/deps/guessing_game-c5678fcca298b7d8.d
@@ -0,0 +1,5 @@
+c:\Users\Graph\Desktop\rustlings_topic\2 guessing_game\target\rls\debug\deps\guessing_game-c5678fcca298b7d8.rmeta: src\main.rs
+
+c:\Users\Graph\Desktop\rustlings_topic\2 guessing_game\target\rls\debug\deps\guessing_game-c5678fcca298b7d8.d: src\main.rs
+
+src\main.rs:
