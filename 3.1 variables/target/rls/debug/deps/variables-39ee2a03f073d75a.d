33352299cfaebea2bc559d8afdc5c86e6d32b40d
Chen Chou <39699466+chenkuochou@users.noreply.github.com>
2022-01-13 22:01:47 +1030

    .

---
+++ 3.1 variables/target/rls/debug/deps/variables-39ee2a03f073d75a.d
@@ -0,0 +1,5 @@
+/Users/chen/Desktop/learn/rustlings_topic/3.1 variables/target/rls/debug/deps/variables-39ee2a03f073d75a.rmeta: src/main.rs
+
+/Users/chen/Desktop/learn/rustlings_topic/3.1 variables/target/rls/debug/deps/variables-39ee2a03f073d75a.d: src/main.rs
+
+src/main.rs:
