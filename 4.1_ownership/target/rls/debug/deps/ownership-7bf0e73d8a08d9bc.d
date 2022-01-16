f808ac04b2bbf0c57c2be9814280361ab30226b1
Chen Chou <39699466+chenkuochou@users.noreply.github.com>
2022-01-16 13:49:48 +1030

    .

---
+++ 4.1_ownership/target/rls/debug/deps/ownership-7bf0e73d8a08d9bc.d
@@ -0,0 +1,5 @@
+/Users/chen/Desktop/learn/rustlings_topic/4.1_ownership/target/rls/debug/deps/ownership-7bf0e73d8a08d9bc.rmeta: src/main.rs
+
+/Users/chen/Desktop/learn/rustlings_topic/4.1_ownership/target/rls/debug/deps/ownership-7bf0e73d8a08d9bc.d: src/main.rs
+
+src/main.rs:
